4f9c246ecf35b7fc43bcfa14cf0c60b4dd88cc69
writepro4 <writepro4@naver.com>
2021-03-19 13:53:47 +0900

    [Update] flipclock 일단 보류

--- assets/js/flipCard.js
+++ assets/js/flipCard.js
@@ -1,9 +1,10 @@
 $(document).ready(function () {
-        FlipClock.Lang.Custom = { days:'Days', hours:'Hours', minutes:'Minutes', seconds:'Seconds' };
+        FlipClock.Lang.Custom = { days:' ', hours:' ', minutes:' ', seconds:' ' };
         var opts = {
             clockFace: 'DailyCounter',
             countdown: true,
-            language: 'Custom'
+            language: 'Custom',
+            size:"sm"
         };
         var countdown = 1616135700 - ((new Date().getTime())/1000); // from: 03/19/2021 03:35 pm +0900
         countdown = Math.max(1, countdown);
